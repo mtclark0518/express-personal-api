fccfc4d5282b21a01fa2617249623ba7b87a3084
mtclark0518 <mtclark0518@gmail.com>
2017-07-26 16:23:04 -0600

    modifies server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // require express and other modules
 var express = require('express'),
     app = express();
+    ejs = require('ejs');
 
 // parse incoming urlencoded form data
 // and populate the req.body object
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 /************
  * DATABASE *
@@ -38,10 +40,10 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/mtclark0518/express-personal-api/blob/master/README.md",
+    base_url: "https://aqueous-waters-33999.herokuapp.com", // CHANGE ME
+    
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
